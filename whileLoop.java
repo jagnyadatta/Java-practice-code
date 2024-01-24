9390f34c8725f1f60873ac45cd5681aafae7679d
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-24 19:33:23 +0530

    while loop comment added

--- whileLoop.java
+++ whileLoop.java
@@ -1,10 +1,10 @@
 public class whileLoop {
     public static void main(String[] args) {
         int i = 0;
-        while(i<11)
+        while(i<11)          //while loop
         {
             System.out.println(i);
-            i = i +1;
+            i = i +1;        //i++
         }
     }
 }
