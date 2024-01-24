5bc2fff0198e07cd0f701095aeafe0053115f81d
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-24 19:25:32 +0530

    comment added

--- forLoop.java
+++ forLoop.java
@@ -1,6 +1,6 @@
 public class forLoop {
     public static void main(String[] args) {
-        for(int count=0;count<=100;count++)
+        for(int count=0;count<=100;count++) //Count++ => Count + 1;
         {
             System.out.println("Hello World!");
         }
