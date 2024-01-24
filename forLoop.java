cc17c063cc51629f82250f60decfaaf5d84afc28
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-24 19:22:51 +0530

    for loop e.g

---
+++ forLoop.java
@@ -0,0 +1,9 @@
+public class forLoop {
+    public static void main(String[] args) {
+        for(int count=0;count<=100;count++)
+        {
+            System.out.println("Hello World!");
+        }
+    
+    }
+}
