c67a0aa1df4c5acd63136f067f40498d49f97874
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-26 20:58:33 +0530

    Do while Loop

---
+++ doWhileLoop.java
@@ -0,0 +1,9 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        do{                                //do statements
+            System.out.println(i);
+            i = i +1;        //i++
+        }while(i<11);
+    }
+}
