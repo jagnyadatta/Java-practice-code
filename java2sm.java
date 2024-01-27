ae2b60588f6ca846b435ce38ef06c0109a4128d8
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-27 13:46:26 +0530

    simple input output code

---
+++ java2sm.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class java2sm {
+    public static void main(String[] args) {
+        System.out.println("Enter Your name :");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("You Entered :"+sc);
+        Scanner sp = new Scanner(System.in);
+        System.out.println("You Entered :"+sp);
+    }
+}
