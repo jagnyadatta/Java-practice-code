155c5e3d378aed34dab8c2e7be7e785bf27f7afb
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-28 19:21:20 +0530

    code changed

--- ifelse.java
+++ ifelse.java
@@ -1,8 +1,8 @@
-import java.util.*;
+import java.util.*;     //java package
 public class ifelse{
     public static void main(String[] args)
 {
-    try (Scanner sc = new Scanner(System.in)) {
+    try (Scanner sc = new Scanner(System.in)) {     //scanner
         int age = sc.nextInt();
         if(age > 18){
             System.out.println("Adult");
