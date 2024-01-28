1b6e33af59715ce4f16c9549b3ee0eaf347c2da1
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-28 19:19:21 +0530

    updated

--- addTwoNum.java
+++ addTwoNum.java
@@ -1,11 +1,11 @@
-import java.util.*;
+import java.util.*;      //java package
 public class addTwoNum{
     public static void main(String[] args){ 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter The Two Numbers :");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        sc.close();
+        sc.close();            //scanner close
         int sum = a + b;
         System.out.println(sum);
         
