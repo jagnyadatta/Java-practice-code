e378b41ad20cac82faf900a0f76fc2fa714e8257
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-04-09 23:35:57 +0530

    updated

---
+++ Alpha/Character_pattern.java
@@ -0,0 +1,17 @@
+
+public class Character_pattern {
+    public static void main(String args[]){
+        int n = 7;
+        char ch = 'A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+                if(ch > 'Z'){
+                    break;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
--- Alpha/Pattern.java
+++ Alpha/Pattern.java
@@ -1,3 +1,4 @@
+
 public class Pattern{
     public static void main(String args[]){
         int n = 5;
