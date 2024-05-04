1996fcac7207395ac4421e8252b8a429a460ac62
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-05-04 16:21:17 +0530

    recursion 1

--- Alpha/Recursion/PrintNum.java
+++ Alpha/Recursion/PrintNum.java
@@ -1,5 +1,5 @@
 public class PrintNum{
-    //Recursive function 
+    //Recursive function (Decreasing Order)
     public static void printDec(int n){
         if(n == 1){
             System.out.println(n);
@@ -8,8 +8,19 @@ public class PrintNum{
         System.out.print(n+ " ");
         printDec(n-1);    //recursive function called
     }
+
+    //Recursive function (Increasing Order)
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);    //recursive function called
+        System.out.print(n+" ");
+    }
     public static void main(String args[]){
         int n = 10;
         printDec(n);
+        printInc(n);
     }
 }
