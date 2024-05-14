15351b09f5bd8d27c2385e6149fc5d865f5d93d5
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-05-14 20:18:37 +0530

    updated

--- Alpha/Recursion/FriendsPairing.java
+++ Alpha/Recursion/FriendsPairing.java
@@ -14,6 +14,7 @@ public class FriendsPairing {
         //totWays
         int totWays = fnm1 + pairWays;
         return totWays;
+        //return friendPair(n-1)+(n-1)*friendPair(n-2);
     }
 
     public static void main(String args[]){
