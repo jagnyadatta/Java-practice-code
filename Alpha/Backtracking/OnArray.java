16bf6cf0ce42cfc2e83e3463f400f7f27ad39a33
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-10-28 21:11:43 +0530

    Array on backtracking

---
+++ Alpha/Backtracking/OnArray.java
@@ -0,0 +1,24 @@
+public class OnArray {
+    public static void ChangeArr(int arr[], int i, int val){
+        //base case
+        if(i == arr.length){
+            PrintArr(arr);
+            return;
+        }
+        //recursion
+        arr[i]=val;
+        ChangeArr(arr, i+1, val+1);    //Function call step
+        arr[i] = arr[i]-2;         //Backtracking step
+    }
+    public static void PrintArr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = new int[5];
+        ChangeArr(arr, 0, 1);
+        PrintArr(arr);
+    }
+}
