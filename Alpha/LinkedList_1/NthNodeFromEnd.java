971f1caf034bf0125702b985245b0068926189e4
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-01-26 20:08:58 +0530

    Nth Node From End in LL

---
+++ Alpha/LinkedList_1/NthNodeFromEnd.java
@@ -0,0 +1,40 @@
+public class NthNodeFromEnd {
+    public static class Node{
+        int data;
+        Node next;
+        Node (int data){
+            this.data = data;
+        }
+    }
+
+    //nth Node function 
+    public static Node nthNode(Node head, int n){
+        int size = 0;
+        Node temp = head;
+        while(temp != null){
+            size++;
+            temp = temp.next;
+        }
+        int a = size - n + 1;
+        temp = head;
+        for(int i = 1; i<a; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+    public static void main(String[] args){
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        Node temp = nthNode(a,1);
+        System.out.println(temp.data);
+    }
+}
