22a47bff3e46bd04e0ae73beb17de0586b7e12c7
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-02-15 13:09:05 +0530

    updated Infix java code

--- Alpha/Stack/Infix.java
+++ Alpha/Stack/Infix.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class Infix {
     public static void main(String[] args){
-        String str= "9-5+3*4/6";
+        String str= "9-(5+3)*4/8";
         Stack<Integer> val = new Stack<>();
         Stack<Character> op = new Stack<>();
         for(int i = 0; i<str.length(); i++){
@@ -10,8 +10,19 @@ public class Infix {
             int ascii = (int)ch;
             if(ascii >= 48 && ascii <= 57){
                 val.push(ascii-48);
-            }else if(op.size()== 0){
+            }else if(op.size()== 0 || ch == '(' || op.peek() == '('){
                 op.push(ch);
+            }else if(ch == ')'){
+                while(op.peek() != '('){
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    if(op.peek() == '+')val.push(v1+v2);
+                    if(op.peek() == '-')val.push(v1-v2);
+                    if(op.peek() == '*')val.push(v1*v2);
+                    if(op.peek() == '/')val.push(v1/v2);
+                    op.pop();
+                }
+                op.pop();
             }else{
                 if(ch == '+' || ch == '-'){
                     //work
