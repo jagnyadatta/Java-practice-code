4fe5c53c5bcc2c508ca8dcd6de4d12ccbfbeb4c3
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-10-31 22:57:02 +0530

    Learned Pre,In,Post Order tree Print

--- Alpha/BinaryTree_1/BuildTreePreOrder.java
+++ Alpha/BinaryTree_1/BuildTreePreOrder.java
@@ -11,7 +11,7 @@ public class BuildTreePreOrder {
     }
     static class BinaryTree{
         static int idx = -1;
-        public static Node buildTree(int nodes[]){
+        public Node buildTree(int nodes[]){
             idx++;
             if(nodes[idx] == -1) return null;
             Node newNode = new Node(nodes[idx]);
@@ -19,11 +19,40 @@ public class BuildTreePreOrder {
             newNode.right = buildTree(nodes);
             return newNode;
         }
+        //PRE-ORDER TREE PRINT
+        public void preOrder(Node root){
+            if(root == null){
+                // System.out.print("-1 ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+        //IN-ORDER TREE PRINT
+        public void inOrder(Node root){
+            if(root == null) return;
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+        //POST-ORDER TREE PRINT
+        public void postOrder(Node root){
+            if(root==null) return;
+            postOrder(root.left);
+            preOrder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
     public static void main(String[] args){
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+        tree.preOrder(root);
+        System.out.println();
+        tree.inOrder(root);
+        System.out.println();
+        tree.postOrder(root);
     }
 }
